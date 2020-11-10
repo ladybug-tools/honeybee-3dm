671dd72dd9bbd172d379acd49a5d25b7dfb76a7d
Devang <devngc@gmail.com>
2020-11-09 20:16:39 -0500

    refactor(tests) : relative path added and assets folder created

--- tests/room_test.py
+++ tests/room_test.py
@@ -1,11 +1,9 @@
 import pytest
-import rhino3dm
-from pathlib import Path
 from honeybee.room import Room
 from honeybee_3dm.room import to_room
 
 
 def test_room():
-    path = str((Path.cwd()).joinpath('tests', 'test.3dm'))
-    room = to_room(path)[0]
+    relative_path = './tests/assets/test.3dm'
+    room = to_room(relative_path)[0]
     assert isinstance(room, Room)
