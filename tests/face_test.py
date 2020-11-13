e13ce3ec9d468470ddefa70a687f32734e06126b
Devang <devngc@gmail.com>
2020-11-12 19:59:55 -0500

    test(aperture): Initial test for Honeybee aperture

--- tests/face_test.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from honeybee.face import Face
-from honeybee_3dm.face import to_face
-
-
-def test_face():
-    relative_path = './tests/assets/test.3dm'
-    room = to_face(relative_path)[0]
-    assert isinstance(room, Face)
