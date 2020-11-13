3f2f5b628e79e8b7950b7afcfa20190d0f7103ba
Devang <devngc@gmail.com>
2020-11-12 20:28:29 -0500

    test(aperture): Refactoring

--- tests/aperture_test.py
+++ tests/aperture_test.py
@@ -9,12 +9,11 @@ model = to_model(path)
 def test_apertures():
     # Check for User provided name
     assert 'Window East' in [aperture.display_name for aperture in model.apertures]
-    # Check for Vertices
+    # Check for vertices & normal
     aperture = [
         aperture for aperture in model.apertures if aperture.display_name == 'Window East']
     assert aperture[0].vertices[0].is_equivalent(Point3D(5, 2.615265, 0.296184), 0.01)
     assert aperture[0].vertices[1].is_equivalent(Point3D(5, 7.105553, 0.296184), 0.01)
     assert aperture[0].vertices[2].is_equivalent(Point3D(5, 7.105553, 2.769152), 0.01)
     assert aperture[0].vertices[3].is_equivalent(Point3D(5, 2.615265, 2.769152), 0.01)
-    # Check for Normal
     assert aperture[0].normal == Vector3D(1, 0, 0)
--- tests/room_test.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from honeybee.room import Room
-from honeybee_3dm.room import to_room
-
-
-def test_room():
-    relative_path = './tests/assets/test.3dm'
-    room = to_room(relative_path)[0]
-    assert isinstance(room, Room)
