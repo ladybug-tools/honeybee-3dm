65dd414af244b0eb8ba492b1881b2bfba34c2814
Devang <devngc@gmail.com>
2020-11-16 16:24:39 -0500

    refactro(face): Refactored to make excepton handling specific

--- honeybee_3dm/togeometry.py
+++ honeybee_3dm/togeometry.py
@@ -155,78 +155,79 @@ def brep2d_to_face3d(brep, tolerance):
         A list of Ladybug Face3D objects.
     """
     faces = []
-    try:
-        # * Check 01 - If any of the edge is an arc
-        lines = []
-        not_line = 0
+    # * Check 01 - If any of the edge is an arc
+    lines = []
+    not_line = 0
+    for i in range(len(brep.Edges)):
+        if brep.Edges[i].IsLinear(tolerance) is False:
+            not_line += 1
+    # If there's an arc in one of the edges, mesh it
+    if not_line > 0:
+        warnings.warn(
+            "The surface has curved edges. It will be meshed.")
+        faces.extend(brep_to_face3d(brep))
+        return faces
+    else:
+        # Create Ladybug lines from start and end points of edges
         for i in range(len(brep.Edges)):
-            if brep.Edges[i].IsLinear(tolerance) == False:
-                not_line += 1
-        # If there's an arc in one of the edges, mesh it
-        if not_line > 0:
-            warnings.warn(
-                "The surface has curved edges. It will be meshed.")
-            faces.extend(brep_to_face3d(brep))
-        else:
-            # Create Ladybug lines from start and end points of edges
-            for i in range(len(brep.Edges)):
-                start_pt = to_point3d(brep.Edges[i].PointAtStart)
-                end_pt = to_point3d(brep.Edges[i].PointAtEnd)
-                line = LineSegment3D.from_end_points(start_pt, end_pt)
-                lines.append(line)
+            start_pt = to_point3d(brep.Edges[i].PointAtStart)
+            end_pt = to_point3d(brep.Edges[i].PointAtEnd)
+            line = LineSegment3D.from_end_points(start_pt, end_pt)
+            lines.append(line)
 
-            # Create Ladybug Polylines from the lines
-            polylines = Polyline3D.join_segments(lines, tolerance)
-            # Sort the Ladybug Polylines based on length
-            # The longest polyline belongs to the boundary of the face
-            # The rest of the polylines belong to the holes in the face
-            sorted_polylines = sorted(polylines, key=lambda polyline: polyline.length)
-            # The first polyline in the list shall always be the polyline for
-            # the boundary
-            sorted_polylines.reverse()
+        # Create Ladybug Polylines from the lines
+        polylines = Polyline3D.join_segments(lines, tolerance)
+        # Sort the Ladybug Polylines based on length
+        # The longest polyline belongs to the boundary of the face
+        # The rest of the polylines belong to the holes in the face
+        sorted_polylines = sorted(polylines, key=lambda polyline: polyline.length)
+        # The first polyline in the list shall always be the polyline for
+        # the boundary
+        sorted_polylines.reverse()
 
-            # Getting all vertices of the face
-            mesh = brep.Faces[0].GetMesh(rhino3dm.MeshType.Any)
+        # Getting all vertices of the face
+        mesh = brep.Faces[0].GetMesh(rhino3dm.MeshType.Any)
+        try:
             mesh_pts = [to_point3d(mesh.Vertices[i])
                         for i in range(len(mesh.Vertices))]
-
-            # In the list of the Polylines, if there's only one polyline then
-            # the face has no holes
-            if len(sorted_polylines) == 1:
-                if len(mesh_pts) == 4 or len(mesh_pts) == 3:
-                    faces.extend(mesh_to_face3d(mesh))
-                else:
-                    boundary_pts = remove_dup_vertices(
-                        sorted_polylines[0].vertices, tolerance)
-                    lb_face = Face3D(boundary=boundary_pts)
-                    faces.append(lb_face)
-
-            # In the list of the Polylines, if there's more than one polyline then
-            # the face has hole / holes
-            elif len(sorted_polylines) > 1:
+        except AttributeError:
+            warnings.warn(
+                'Non-planar Breps are not handled by Honeybee.'
+            )
+            return faces
+        # In the list of the Polylines, if there's only one polyline then
+        # the face has no holes
+        if len(sorted_polylines) == 1:
+            if len(mesh_pts) == 4 or len(mesh_pts) == 3:
+                faces.extend(mesh_to_face3d(mesh))
+            else:
                 boundary_pts = remove_dup_vertices(
                     sorted_polylines[0].vertices, tolerance)
-                hole_pts = [remove_dup_vertices(polyline.vertices, tolerance)
-                            for polyline in sorted_polylines[1:]]
-                # Merging lists of hole_pts
-                total_hole_pts = [
-                    pts for pts_lst in hole_pts for pts in pts_lst]
-                hole_pts_on_boundary = [
-                    pts for pts in total_hole_pts if pts in boundary_pts]
-                # * Check 02 - If any of the hole is touching the boundary of the face
-                # then mesh it
-                if len(hole_pts_on_boundary) > 0:
-                    warnings.warn(
-                        "The surface has holes that touch the boundary. \
-                            It will be meshed")
-                    faces.extend(brep_to_face3d(brep))
-                else:
-                    lb_face = Face3D(boundary=boundary_pts, holes=hole_pts)
-                    faces.append(lb_face)
-    # If any of the above fails, mesh it
-    except Exception as e:
-        print(str(e) + "Face creation failed, this face will be meshed")
-        faces.extend(brep_to_face3d(brep))
+                lb_face = Face3D(boundary=boundary_pts)
+                faces.append(lb_face)
+
+        # In the list of the Polylines, if there's more than one polyline then
+        # the face has hole / holes
+        elif len(sorted_polylines) > 1:
+            boundary_pts = remove_dup_vertices(
+                sorted_polylines[0].vertices, tolerance)
+            hole_pts = [remove_dup_vertices(polyline.vertices, tolerance)
+                        for polyline in sorted_polylines[1:]]
+            # Merging lists of hole_pts
+            total_hole_pts = [
+                pts for pts_lst in hole_pts for pts in pts_lst]
+            hole_pts_on_boundary = [
+                pts for pts in total_hole_pts if pts in boundary_pts]
+            # * Check 02 - If any of the hole is touching the boundary of the face
+            # then mesh it
+            if len(hole_pts_on_boundary) > 0:
+                warnings.warn(
+                    "The surface has holes that touch the boundary. \
+                        It will be meshed")
+                faces.extend(brep_to_face3d(brep))
+            else:
+                lb_face = Face3D(boundary=boundary_pts, holes=hole_pts)
+                faces.append(lb_face)
 
     return faces
 
