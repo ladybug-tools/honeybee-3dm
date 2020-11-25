42971fdc19717364f632aa547678a2947431a577
Devang <devngc@gmail.com>
2020-11-25 10:08:31 -0500

    feat(room): Add capability to use a config file

--- honeybee_3dm/room.py
+++ honeybee_3dm/room.py
@@ -13,7 +13,7 @@ from ladybug_geometry.geometry3d.polyface import Polyface3D
 
 # Importing dependencies from Honeybee-3dm package
 from .togeometry import to_face3d
-from .helper import filter_objects_by_layer, HB_layers
+from .helper import objects_on_parent_child, HB_layers
 
 
 def import_rooms(rhino3dm_file, tolerance=None, visibility=True, config=None):
@@ -34,42 +34,43 @@ def import_rooms(rhino3dm_file, tolerance=None, visibility=True, config=None):
     Returns:
         A list of Honeybee rooms.
     """
-    # rooms from rhino file
-    try:
-        volumes = filter_objects_by_layer(rhino3dm_file, HB_layers.room.value,
-            visibility=visibility)
-    except ValueError:
-        # no layer named room
-        return []
-
-    # From all the objects on the layer named "room" take only solid objects
-    closed_volumes = []
-    for vol in volumes:
-        geo = vol.Geometry
+    hb_rooms = []
+    config = config
+    # If config file is provided and any Rhino layer is tied to HB_room layer
+    if config and config['HB_layers']['HB_room']:
+        solid_objs = objects_on_parent_child(rhino3dm_file,
+            config['HB_layers']['HB_room'], visibility=visibility)
+    
+    # Take only solid objects
+    solids = []
+    for solid in solid_objs:
+        geo = solid.Geometry
         if isinstance(geo, rhino3dm.Extrusion) and geo.IsSolid:
-            closed_volumes.append(vol)
+            solids.append(solid)
         elif isinstance(geo, rhino3dm.Brep) and geo.IsSolid:
-            closed_volumes.append(vol)
+            solids.append(solid)
         elif isinstance(geo, rhino3dm.Mesh) and geo.IsClosed:
-            closed_volumes.append(vol)
+            solids.append(solid)
 
-    if len(closed_volumes) != len(volumes):
-        diff = len(volumes) - len(closed_volumes)
+    # Report if there are non-slid objects on the layer
+    if len(solid_objs) != len(solids):
+        diff = len(solid_objs) - len(solids)
+        layer_name = config['HB_layers']['HB_room']
         warnings.warn(
-            f'{diff} objects on the "room" layer are not closed Brep or Meshes. '
+            f'{diff} objects on the "{layer_name}" layer are not closed Brep or Meshes. '
             ' These objects will be ignored.'
         )
-
-    hb_rooms = []
-    # Covert solids into Ladybug Face3D objects
-    for volume in closed_volumes:
-        lb_faces = to_face3d(volume, tolerance=tolerance)
-        # Create Ladybug Polyface3D object from Ladybug Face3D objects
+    for solid in solids:
+        try:
+            lb_faces = to_face3d(solid, tolerance=tolerance)
+        except AttributeError:
+            warnings.warn(
+                'Please turn on the shaded mode in rhino, save the file, and try again.'
+            )
+            continue
         lb_polyface = Polyface3D.from_faces(lb_faces, tolerance)
-        # Assign name
-        name = volume.Attributes.Name or clean_and_id_string('room')
-        # Create Honeybee Room object from Ladybug Polyface3D object
+        name = solid.Attributes.Name or clean_and_id_string('Room')
         hb_room = Room.from_polyface3d(name, lb_polyface)
         hb_rooms.append(hb_room)
-
+  
     return hb_rooms
