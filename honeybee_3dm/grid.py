29dfd360a5e692b2890c2e878bafb68685cee6b4
Devang <devngc@gmail.com>
2020-11-25 00:15:46 -0500

    feat(grid): Use a config file

--- honeybee_3dm/grid.py
+++ honeybee_3dm/grid.py
@@ -18,7 +18,7 @@ from .helper import filter_objects_by_layer, HB_layers
 
 
 def import_grids(rhino3dm_file, tolerance, grid_size=1, grid_offset=0, *,
-    visibility=True):
+    visibility=True, config=None):
     """Creates Honeybee grids from a rhino3dm file.
 
     This function assumes all the grid objects are under a layer named ``grid``.
@@ -30,46 +30,39 @@ def import_grids(rhino3dm_file, tolerance, grid_size=1, grid_offset=0, *,
         grid_offset: Grid offset from the grid geometry. Defaults to 0.
         visibility: Bool. If set to False then the objects on an "off"
             layer in Rhino3dm will also be imported. Defaults to True.
-
+        config: A dictionary. The config file as a dictionary object.
+            Defaults to None.
     Returns:
         A list of Honeybee grids.
     """
-
-    # Objects from rhino file
-    try:
-        grid_objs = filter_objects_by_layer(rhino3dm_file, HB_layers.grid.value,
-            visibility=visibility)
-        warnings.warn(
-            'Only objects on Honeybee layer "grid" will be used to create grids.'
-            ' Grids will not be imported if the layer is "Off" in rhino.'
-            )
-    except ValueError:
-        # no layer named grid
-        return []
-
     hb_grids = []
+    config = config
+    # If config file is provided and any Rhino layer is tied to HB_grid layer
+    if config and config['HB_layers']['HB_grid']:
+        grid_objs = filter_objects_by_layer(rhino3dm_file, 
+            config['HB_layers']['HB_grid'], visibility=visibility)
+        
+        for obj in grid_objs:
+            geo = obj.Geometry
+            # If it's a Brep
+            if isinstance(geo, rhino3dm.Brep):
+                # Check if the Brep is planar
+                if check_planarity(geo):
+                    mesh3d = brep_to_face3d(geo, tolerance).mesh_grid(grid_size,
+                        grid_size, grid_offset)
+                else:
+                    warnings.warn(
+                        'A Non-planar Brep is not handled by Honeybee for grids.' 
+                        ' It is ignored.'
+                    )
+                    continue
+            # If it's a Mesh
+            elif isinstance(geo, rhino3dm.Mesh):
+                mesh3d = mesh_to_mesh3d(geo)
 
-    for obj in grid_objs:
-        geo = obj.Geometry
-        # If it's a Brep
-        if isinstance(geo, rhino3dm.Brep):
-            # Check if the Brep is planar
-            if check_planarity(geo):
-                mesh3d = brep_to_face3d(geo, tolerance).mesh_grid(grid_size,
-                    grid_size, grid_offset)
-            else:
-                warnings.warn(
-                    'A Non-planar Brep is not handled by Honeybee for grids.' 
-                    ' It is ignored.'
-                )
-                continue
-        # If it's a Mesh
-        elif isinstance(geo, rhino3dm.Mesh):
-            mesh3d = mesh_to_mesh3d(geo)
-
-        name = obj.Attributes.Name
-        obj_name = name or clean_and_id_string('grid')
-        args = [clean_string(obj_name), mesh3d]
-        hb_grids.append(SensorGrid.from_mesh3d(*args))
+            name = obj.Attributes.Name
+            obj_name = name or clean_and_id_string('grid')
+            args = [clean_string(obj_name), mesh3d]
+            hb_grids.append(SensorGrid.from_mesh3d(*args))
 
     return hb_grids
