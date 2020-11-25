85ecd9a1c5de0bfdc5d1a2e1adc64e7bfbb4f0a9
Devang <devngc@gmail.com>
2020-11-25 13:17:42 -0500

    feat(grid): Add grid controls from config file

--- honeybee_3dm/grid.py
+++ honeybee_3dm/grid.py
@@ -10,15 +10,14 @@ from honeybee.typing import clean_and_id_string, clean_string
 
 # Importing dependencies from Honeybee-3dm package
 from .togeometry import brep_to_face3d, mesh_to_mesh3d, check_planarity
-from .helper import objects_on_parent_child, HB_layers
+from .helper import objects_on_parent_child, HB_layers, check_grid_controls
 
 
 # TODO: expose an option to change the target layer name
 # TODO: Grid size and offset will have to provided using the config file
 
 
-def import_grids(rhino3dm_file, tolerance, grid_size=1, grid_offset=0, *,
-    visibility=True, config=None):
+def import_grids(rhino3dm_file, tolerance, *, visibility=True, config=None):
     """Creates Honeybee grids from a rhino3dm file.
 
     This function assumes all the grid objects are under a layer named ``grid``.
@@ -26,8 +25,6 @@ def import_grids(rhino3dm_file, tolerance, grid_size=1, grid_offset=0, *,
     Args:
         rhino3dm_file: The rhino file from which Honeybee grids will be created.
         tolerance: A rhino3dm tolerance object. Tolerance set in the rhino file.
-        grid_size: Grid size. Defaults to 1.
-        grid_offset: Grid offset from the grid geometry. Defaults to 0.
         visibility: Bool. If set to False then the objects on an "off"
             layer in Rhino3dm will also be imported. Defaults to True.
         config: A dictionary. The config file as a dictionary object.
@@ -39,6 +36,23 @@ def import_grids(rhino3dm_file, tolerance, grid_size=1, grid_offset=0, *,
     config = config
     # If config file is provided and any Rhino layer is tied to HB_grid layer
     if config and config['HB_layers']['HB_grid']:
+        
+        # Grid controls
+        if config['grid_controls']:
+            # grid_size_x, grid_size_y, grid_offset = tuple([config['grid_controls'][k]
+            #     for k in config['grid_controls']])
+            grid_settings = [config['grid_controls'][k]
+                for k in config['grid_controls']]
+            if check_grid_controls(grid_settings):
+                grid_controls = grid_settings
+            else:
+                warnings.warn(
+                    'Grid setting from config file not applied.'
+                    ' Please make sure grid controls are floating point numbers'
+                    ' and try again.'
+                )
+                grid_controls = [1.0, 1.0, 0.0]
+
         grid_objs = objects_on_parent_child(rhino3dm_file, 
             config['HB_layers']['HB_grid'], visibility=visibility)
         
@@ -49,8 +63,8 @@ def import_grids(rhino3dm_file, tolerance, grid_size=1, grid_offset=0, *,
                 # Check if the Brep is planar
                 if check_planarity(geo):
                     try:
-                        mesh3d = brep_to_face3d(geo, tolerance).mesh_grid(grid_size,
-                            grid_size, grid_offset)
+                        mesh3d = brep_to_face3d(geo, tolerance).mesh_grid(grid_controls[0],
+                            grid_controls[1], grid_controls[2])
                     except AttributeError:
                         warnings.warn(
                             'Please turn on the shaded mode in rhino, save the file,'
@@ -68,7 +82,7 @@ def import_grids(rhino3dm_file, tolerance, grid_size=1, grid_offset=0, *,
                 mesh3d = mesh_to_mesh3d(geo)
 
             name = obj.Attributes.Name
-            obj_name = name or clean_and_id_string('grid')
+            obj_name = name or clean_and_id_string('Grid')
             args = [clean_string(obj_name), mesh3d]
             hb_grids.append(SensorGrid.from_mesh3d(*args))
 
