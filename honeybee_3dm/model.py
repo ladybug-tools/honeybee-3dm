ab8739ebe730c97204ea1cc0a28dd0abbc78a021
Devang <devngc@gmail.com>
2020-11-26 17:11:12 -0500

    feat(model): Add capability to use a .mat file

--- honeybee_3dm/model.py
+++ honeybee_3dm/model.py
@@ -15,11 +15,9 @@ from .room import import_rooms
 from .face import import_faces
 from .grid import import_grids
 from .helper import get_unit_system, read_json
+from .material import mat_to_dict
 
-
-# TODO: Add an argument to let the user choose the layers and assign energy and radiance
-# properties if desired.
-def import_3dm(path, name=None, *, config_path=None, visibility=True):
+def import_3dm(path, name=None, *, config_path=None, mat_path=None, visibility=True):
     """Import a rhino3dm file as a Honeybee model.
 
     This function outputs a Honeybee model from the faces, shades, apertures, and doors
@@ -33,12 +31,31 @@ def import_3dm(path, name=None, *, config_path=None, visibility=True):
             model. Default will be the same as Rhino file name.
         config_path: A text string path to the config file on disk.
             Defaults to not using a config file.
+        mat_path: A text string path to the .mat file that holds radiance modifiers.
+            Defaults to not using a .mat file.
         visibility: Bool. If set to False then the objects on an "off"
             layer in Rhino3dm will also be imported. Defaults to True.
 
     Returns:
         A Honeybee model.
     """
+    # RHINO FILE
+    if not os.path.isfile(path):
+        warnings.warn(
+            f'The path {path} is not a valid path. Please try using "r" prefix to'
+            ' the file path.'
+                        )
+        return []
+    name = name or '.'.join(os.path.basename(path).split('.')[:-1])
+    name = clean_string(name)
+    rhino3dm_file = rhino3dm.File3dm.Read(path)
+    if not rhino3dm_file:
+        raise ValueError(f'Input Rhino file: {path} returns None object.')
+    warnings.warn(
+        f'*****The rhino file MUST be saved in the SHADED mode for'
+        ' Honeybee to work.*****'
+                    )
+
     # CONFIG FILE
     if config_path:
         os.path.isfile(config_path)
@@ -53,33 +70,29 @@ def import_3dm(path, name=None, *, config_path=None, visibility=True):
             return []
     else:
         config = None
+    
+    # RADIANCE MATERIAL FILE
+    if mat_path:
+        os.path.isfile(mat_path)
+        # Get the config file as a directory
+        try:
+            modifiers_dict = mat_to_dict(mat_path)
+        except OSError:
+            warnings.warn(
+                f'The path {mat_path} is not a valid path. Please try' 
+                ' using "r" prefix to the file path.'
+                    )
+            return []
+    else:
+        modifiers_dict = None
 
-
-    # RHINO FILE
-    if not os.path.isfile(path):
-        warnings.warn(
-            f'The path {path} is not a valid path. Please try using "r" prefix to'
-            ' the file path.'
-        )
-        return []
-    name = name or '.'.join(os.path.basename(path).split('.')[:-1])
-    name = clean_string(name)
-    rhino3dm_file = rhino3dm.File3dm.Read(path)
-    if not rhino3dm_file:
-        raise ValueError(f'Input Rhino file: {path} returns None object.')
-    warnings.warn(
-        f'*****The rhino file MUST be saved in the SHADED mode for'
-        ' Honeybee to work.*****'
-    )
-
-    # TODO: RADIANCE.MAT FILE
     model_tolerance = rhino3dm_file.Settings.ModelAbsoluteTolerance
     model_angle_tolerance = rhino3dm_file.Settings.ModelAngleToleranceDegrees
     model_unit = get_unit_system(rhino3dm_file)
 
     # Honeybee Faces, Shades, Apertures and Doors
     hb_faces, hb_shades, hb_apertures, hb_doors = import_faces(
-            rhino3dm_file, model_tolerance, visibility, config)
+            rhino3dm_file, model_tolerance, visibility, config, modifiers_dict)
 
     # Honeybee Grid objects if they are tied to a Honeybee layer in config file
     if config and config['HB_layers']['HB_grid']:
