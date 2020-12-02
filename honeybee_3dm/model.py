8de7418e167b581ad0440e0201f0a710db38b45b
Devang <devngc@gmail.com>
2020-12-02 14:30:27 -0500

    refactor(model): Change to work with latest config file structure

--- honeybee_3dm/model.py
+++ honeybee_3dm/model.py
@@ -1,23 +1,17 @@
 """Creating Honeybee model objects from rhino3dm surfaces and closed volumes"""
 import os
-import warnings
-
-# The Rhino3dm library provides the ability to access content of a Rhino3dm
-# file from outside of Rhino
 import rhino3dm
 
-# Importing core Honeybee dependency
 from honeybee.model import Model
 from honeybee.typing import clean_string
 
-# Importing dependencies from Honeybee-3dm package
-from .room import import_rooms
-from .face import import_faces
-from .grid import import_grids
-from .helper import get_unit_system, read_json
-from .material import mat_to_dict
+from .face import import_objects, import_objects_with_config
+from .helper import get_unit_system
+from .config import read_json, check_config, check_parent_in_config
+from .layer import child_parent_dict
 
-def import_3dm(path, name=None, *, config_path=None, mat_path=None, visibility=True):
+
+def import_3dm(path, name=None, *, config_path=None, visibility=True):
     """Import a rhino3dm file as a Honeybee model.
 
     This function outputs a Honeybee model from the faces, shades, apertures, and doors
@@ -31,8 +25,6 @@ def import_3dm(path, name=None, *, config_path=None, mat_path=None, visibility=T
             model. Default will be the same as Rhino file name.
         config_path: A text string path to the config file on disk.
             Defaults to not using a config file.
-        mat_path: A text string path to the .mat file that holds radiance modifiers.
-            Defaults to not using a .mat file.
         visibility: Bool. If set to False then the objects on an "off"
             layer in Rhino3dm will also be imported. Defaults to True.
 
@@ -41,67 +33,73 @@ def import_3dm(path, name=None, *, config_path=None, mat_path=None, visibility=T
     """
     # RHINO FILE
     if not os.path.isfile(path):
-        warnings.warn(
-            f'The path {path} is not a valid path. Please try using "r" prefix to'
-            ' the file path.'
-                        )
-        return []
-    name = name or '.'.join(os.path.basename(path).split('.')[:-1])
-    name = clean_string(name)
+        raise FileNotFoundError(
+            'The path to rhino file is not valid.'
+            )
     rhino3dm_file = rhino3dm.File3dm.Read(path)
     if not rhino3dm_file:
         raise ValueError(f'Input Rhino file: {path} returns None object.')
 
     # CONFIG FILE
     if config_path:
-        os.path.isfile(config_path)
+        if not os.path.isfile(config_path):
+            raise FileNotFoundError(
+                'The path is not a valid path.'
+                ' If file exists, try using double backslashes in file path'
+                ' and try again.'
+            )
         # Get the config file as a directory
-        try:
-            config = read_json(config_path)
-        except OSError:
-            warnings.warn(
-                f'The path {config_path} is not a valid path. Please try' 
-                ' using "r" prefix to the file path.'
-                    )
-            return []
+        config = read_json(config_path)
     else:
         config = None
-    
-    # RADIANCE MATERIAL FILE
-    if mat_path:
-        os.path.isfile(mat_path)
-        # Get the config file as a directory
-        try:
-            modifiers_dict = mat_to_dict(mat_path)
-        except OSError:
-            warnings.warn(
-                f'The path {mat_path} is not a valid path. Please try' 
-                ' using "r" prefix to the file path.'
-                    )
-            return []
-    else:
-        modifiers_dict = None
 
+    # Extracting unit parameters from rhino
     model_tolerance = rhino3dm_file.Settings.ModelAbsoluteTolerance
     model_angle_tolerance = rhino3dm_file.Settings.ModelAngleToleranceDegrees
     model_unit = get_unit_system(rhino3dm_file)
 
-    # Honeybee Faces, Shades, Apertures and Doors
-    hb_faces, hb_shades, hb_apertures, hb_doors = import_faces(
-            rhino3dm_file, model_tolerance, visibility, config, modifiers_dict)
+    # Place holders
+    hb_rooms, hb_faces, hb_shades, hb_apertures, hb_doors, hb_grids = tuple(
+            [[] for _ in range(6)])
 
-    # Honeybee Grid objects if they are tied to a Honeybee layer in config file
-    if config and config['HB_layers']['HB_grid']:
-        hb_grids = import_grids(rhino3dm_file, model_tolerance, 
-            visibility=visibility, config=config)
-    else:
-        hb_grids = []
+    # A dictionary with child layer : parent layer structure
+    child_to_parent = child_parent_dict(rhino3dm_file)
 
-    # Honeybee Room objects if they are tied to a Honeybee layer in config file
-    if config and config['HB_layers']['HB_room']:
-        hb_rooms = import_rooms(rhino3dm_file, model_tolerance, visibility, config)
+    # If config is provided
+    if config and check_config(rhino3dm_file, config):
+        
+        for layer in rhino3dm_file.Layers:
+            
+            # Import objects from each layer in the config file
+            if layer.Name in config['layers']:
+                hb_objs = import_objects_with_config(
+                    rhino3dm_file, layer, tolerance=model_tolerance,
+                        visibility=visibility, config=config)
+                hb_faces.extend(hb_objs[0])
+                hb_shades.extend(hb_objs[1])
+                hb_apertures.extend(hb_objs[2])
+                hb_doors.extend(hb_objs[3])
+                hb_grids.extend(hb_objs[4])
+
+            # skip child layers that might already have been imported
+            elif check_parent_in_config(rhino3dm_file, config,
+                layer.Name, child_to_parent[layer.Name]):
+                continue
+
+            # Import objects from each layer not in the config file
+            else:
+                hb_faces.extend(import_objects(rhino3dm_file, layer,
+                    tolerance=model_tolerance))
+    
+    # If config is not provided
     else:
-        hb_rooms = []
+        for layer in rhino3dm_file.Layers:
+            hb_faces.extend(import_objects(rhino3dm_file, layer,
+                tolerance=model_tolerance))
+    
+    # Honeybee model name
+    name = name or '.'.join(os.path.basename(path).split('.')[:-1])
+    name = clean_string(name)
 
     # Honeybee Model
     hb_model = Model(
